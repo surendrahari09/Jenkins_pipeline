baa0884874c3340a605f2f71a9be38a96130bd33
Surendra Hari <115442378+surendrahari09@users.noreply.github.com>
2022-11-29 16:38:42 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import time
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"Time of Call Home Page change B": time.time()})
+
+@app.route("/one")
+def one():
+    return jsonify({"Time of Call One": time.ctime()})
+
+@app.route("/two")
+def two():
+    return jsonify({"Time of Call Two": time.time()})
+
+@app.route("/three")
+def three():
+    return jsonify({"Time of Call Three": time.time()})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
